115d4d6ef9c06c9958ba9be6a38ed92cd59792fa
leeyeojin <yjlee@pndtech.co.kr>
2025-03-26 13:54:46 +0900

    내꺼삭제

--- monoBrain/guide/js/helper.js
+++
@@ -1,4 +0,0 @@
-// 클래스제거
-export const removeClass = (array) => {
-  array.forEach((a) => a.classList.remove("active"));
-};
--- monoBrain/guide/js/style.js
+++
@@ -1,9 +0,0 @@
-import { frame } from './frame.js';
-import { navigation } from './navigation.js';
-import { centered } from './centered.js';
-import { header } from './header.js';
-
-frame();
-navigation();
-centered();
-header();
